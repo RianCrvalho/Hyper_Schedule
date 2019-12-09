cd8481720e39cca139d7b6568653aa288c9ae582
RianCrvalho <rian_estudante@hotmail.com>
2019-12-09 08:08:58 -0300

    Modificações

--- BD/agenda_normalizada.sql
+++ BD/agenda_normalizada.sql
@@ -12,13 +12,12 @@ PRIMARY KEY (id_usr)
 
 
 CREATE TABLE contato (
-id_contato INT NOT NULL AUTO_INCREMENT,
-id_usr INT NOT NULL,
-numero_contato VARCHAR (30) NOT NULL,
+id_cont INT NOT NULL AUTO_INCREMENT,
+nome_cont VARCHAR(30) NOT NULL,
+numero_cont VARCHAR (30) NOT NULL,
 endereco VARCHAR (255),
-email_contato VARCHAR (100), 
-PRIMARY KEY (id_contato),
-FOREIGN KEY (id_usr) REFERENCES usuario(id_usr)
+email_cont VARCHAR (100), 
+PRIMARY KEY (id_cont)
 );
 
 
@@ -33,10 +32,10 @@ FOREIGN KEY (id_usr) REFERENCES usuario(id_usr)
 
 
 CREATE TABLE contatos_usuario (
-id_contato INT NOT NULL AUTO_INCREMENT, 
+id_cont INT NOT NULL AUTO_INCREMENT, 
 id_usr INT NOT NULL,
-PRIMARY KEY (id_contato, id_usr),
-FOREIGN KEY (id_contato) REFERENCES contato (id_contato),
+PRIMARY KEY (id_cont, id_usr),
+FOREIGN KEY (id_cont) REFERENCES contato (id_cont),
 FOREIGN KEY (id_usr) REFERENCES usuario (id_usr)
 );
 
