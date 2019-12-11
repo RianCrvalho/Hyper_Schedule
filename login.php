06d325149dcd104efc5a9f8d65b2e547be3289aa
RianCrvalho <rian_estudante@hotmail.com>
2019-12-11 00:25:49 -0300

    Iniciando tela login

---
+++ login.php
@@ -0,0 +1,41 @@
+
+<?php
+
+session_start(); 
+ 
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="lib/bootstrap.css">
+</head>
+<body>
+
+	<style type="text/css">
+		body{margin: 1px 10% 1px 10%}
+	</style>
+	<h2>Agenda</h2>
+	<fieldset>
+		<legend>Login</legend>
+		<form method="POST">     
+       Nome: <input type="text" name="nome_usr" value="">
+       Senha: <input type="text" size='10' name="senha_usr" value="" >
+       <button class='btn btn-info' name="btn" value="btn1"> Entrar </button>
+    </form> 
+  </fieldset>
+  <style>
+    .format{
+      visibility: hidden;
+    }
+  </style>
+  <script>
+     function mostrar(){
+      document.getElementsById('form').style.visibility = 'visible';
+     }  
+</script>
+  </body>
+</html>
+
+</body>
+</html>
