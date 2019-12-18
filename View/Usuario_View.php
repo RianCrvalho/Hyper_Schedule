765cedbabd6e3d50feae3d1f9bbb052d64bc2ffb
whiixel <43453366+whiixel@users.noreply.github.com>
2019-12-18 08:21:07 -0300

    Update Usuario_View.php
    .

--- View/Usuario_View.php
+++ View/Usuario_View.php
@@ -1,108 +1,59 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-	<title>Usuario</title>
-	<link rel="stylesheet" type="text/css" href="../lib/bootstrap.css">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../css/estiloTelaLogin.css">
 </head>
 <body>
-	   <style>
-        body{margin:1px 10% 1px 10%;}
-        }
-        header, footer{
-            margin: auto;
-        }
-    </style>
-
-<header align="center">
-	<h1>Agenda</h1>
-</header>
-<main>
-	 
-	<form method="POST" align="right">
-	<button type="submit" id="btn-enviar" name="btn-sair" class="btn btn-info" value="btn2">Voltar</button>
-	</form>
-	<h2>Anotações:</h2>
-
-<?php
-include("../Control/Notas_Control.php");
-$Usuario_id = $_SESSION['Usuario_id'];
-echo "<h3><p align='right'>Bem vindo Usuario:".@$_SESSION['Usuario_nome']."</p></h3>";
-
-$notas = new Notas_Control();
-$dados= $notas->notView($Usuario_id);
-
-if (isset ( $_POST [ 'btn-add' ])) {
-        $data_nota  =  $_POST ['campo_data'];
-        $nota  =  $_POST ['campo_nota'];
-        $notas->add($Usuario_id,$data_nota, $nota);
+ <header align="center">
+       <h1>Agenda</h1> 
+       <h2>~Login~</h2>
+    </header>
+    <main>
+ <?php 
+    include("../Control/Usuario_Control.php");
+    $usuario = new Usuario_Control(); 
+    if (isset ( $_POST [ 'btn-logar' ])) {
+        $nome_usr  =  $_POST ['campo_usuario'];
+        $senha_usr  =  $_POST ['campo_senha'];
+        $usuario->logar($nome_usr, $senha_usr);
 }
-if($_POST['acao']== "edit"){
-  $id = $_POST['id_nota'];
-  $dadosV = $notas->notView($id);
-  foreach($dadosV as $d){
-    $id1 = $d['campo_id'];
-    $data1 = $d['campo_data'];
-    $nota1 = $d['campo_nota'];
+
+if ((@$_SESSION['cadastrado'])=="erro") {
+                echo "<div class='alert alert-danger' role='alert'> Usuário ou senha incorreta!</div>";
+               
+               
+ } else if((@$_SESSION['cadastrado'])=="certo"){
+    unset($_SESSION['cadastrado']);
+ }
+
   
 
-}
-if(isset($_POST['btn-del'])){
-  $id = $_POST[$d['value']];
-  $notas->delNota($id);
-  echo "<div class='alert alert-primary' role='alert'>
-      This is a primary alert—check it out!
-  </div>";
-}
+?>      <div>
 
-}
-
-if (isset($_POST['btn-sair'])) {
-	session_destroy();
-	header('Location:login.php');
-}
-echo "<table class='table table-striped table-hover' border=1 align=center>";
-echo "<tr>";
-echo "<th class='bg-success'> Data: </th>
-<th class='bg-success'> Nota: </th>
-<th class='bg-success'> Editar</th><th class='bg-success'> Deletar </th>";
-echo "</tr>";
-echo "<tr>";
-foreach($dados as $d){
-    echo "<td>".$d['data_nota']."</td>";
-    echo "<td>".$d['nota']."</td>";
-    echo "<td><a href=?acao=edit&id=".$d['id_nota']."onclick='mostrar()'> Editar </a></td>";
-
-    echo "<td> <form method='POST'><button type='input' name='btn-del' value=".$d['id_nota']." class='btn btn-link'>Deletar</button></form></td></td>";
-    echo "</tr>";
-}
-echo "</table>";
-
-?>
-<div id="div_login" class="container">
+        <div id="div_login" class="container">
             <form method="POST">
                 <div class="form-group">
-                    <input type="hidden" name="campo_id" value="<?php echo @$id1;?>">
-                    <label for="date">Data:</label>
-                    <input type="date" id="date" name="campo_data" class="form-control" value="<?php echo @$data1;?>" required>
+                    <label for="user">Usuário:</label>
+                    <input type="text" id="user" name="campo_usuario" class="form-control" required="">
                 </div>
                 <div class="form-group">
-                    <label for="pass">Nota</label>
-                    <input type="text" id="pass" name="campo_nota" class="form-control" value="<?php echo @$nota1;?>" required>
+                    <label for="pass">Senha:</label>
+                    <input type="password" id="pass" name="campo_senha" class="form-control" required="">
                 </div>
                 <div id="div_buttons">
-                    <button type="submit" id="btn-enviar" name="btn-add" class="btn btn-success" value="btn1">adicionar</button>
-                    <button align='right' type="submit" id="btn-enviar" name="btn-add" class="btn btn-success" value="btn1">Atualizar</button>
+                    <button type="submit" id="btn-enviar" name="btn-logar" value="btn1">Enviar</button>
+                    &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp&nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp&nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp&nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp&nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp
+                    &nbsp<a href="cadastro.php">Cadastre-se</a>
                 </div>
             </form>
         </div>
-        <style>
-</main>
-<footer>
-  <div align="center">
+        </div>
+    </main>
+    <footer>
+        <div align="right">
             <p>2019 &copy; Copyright - Todos os direitos reservados | Calango Voador.</p>
         </div>
-</footer>
-
+    </footer>
 </body>
 </html>
-
